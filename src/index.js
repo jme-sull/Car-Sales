992dcc79eef7c31c5b2c992a61196b90019fcf08
Jamie Sullivan <jamiesullivan@jamies-mbp.lan>
2020-06-10 06:40:41 -0400

    connected to redux store

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom';
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 
+import rootReducer from './reducers'
 
 import App from './App';
-
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
